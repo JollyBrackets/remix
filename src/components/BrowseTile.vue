74c7f373d9b53aecda1e30d6a0a6f4a0e0092b10
maettuu <mangori41@bluewin.ch>
2021-09-25 14:03:15 +0200

    include channel pictures, add tiles

---
+++ src/components/BrowseTile.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-hover v-slot="{ hover }">
+    <div class="relative h-48 w-48 m-3">
+      <div class="h-full w-full bg-cover bg-center"
+           :style="{ backgroundImage:`url(${require('../assets/'+imageSource+'.jpg')})` }">
+      </div>
+      <div v-if="hover" id="channel-tile"
+           class=" absolute inset-0 flex justify-center items-center bg-green-400 bg-opacity-90 text-2xl font-extrabold">
+        {{ channel }}
+      </div>
+    </div>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  name: "BrowseTile",
+  props: {
+    imageSource: {type: String, required: true},
+    channel: {type: String, required: true},
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="h-screen w-full bg-black"></div>
+  <section class="h-full w-full bg-black flex px-20 py-20 flex-col">
+    <h2 class="font-bold text-2xl text-left mb-4 ml-3">Channels</h2>
+    <div class="flex flex-row flex-wrap">
+      <div v-for="(hardSource, index) of hardSources" :key="hardSource">
+        <BrowseTile :imageSource="hardSource" :channel="channels[index]"/>
+      </div>
+    </div>
+    <h2 class="font-bold text-2xl text-left mb-4 ml-3">Sports</h2>
+    <h2 class="font-bold text-2xl text-left mb-4 ml-3">Muscle Groups</h2>
+  </section>
 </template>
 
 <script>
+import BrowseTile from "@/components/BrowseTile";
 export default {
-  name: "Browse"
+  name: "Browse",
+  components: { BrowseTile },
+  data: () => ({
+    hardSources: ["caroline-girvan", "chloe-ting", "fit-for-fun", "growingannanas", "headspace", "heather-robertson", "les-mills", "mady-morrison", "my-life", "pamela-reif", "popsugar-fitness", "tiff-x-dan"],
+    channels: ["Caroline Girvan", "Chloe Ting", "Fit for Fun", "Growing Annanas", "Headspace", "Heather Robertson", "Les Mills", "Mady Morrison", "My Life", "Pamela Reif", "Popsugar Fitness", "Tiff x Dan"]
+  }),
 }
 </script>
 
