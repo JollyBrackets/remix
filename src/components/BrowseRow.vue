e3c31c09c35fd85f0adc078c685f65020b0565a8
maettuu <mangori41@bluewin.ch>
2021-09-26 01:13:41 +0200

    add playlists

--- src/components/BrowseRow.vue
+++ src/components/BrowseRow.vue
@@ -3,7 +3,7 @@
     <h2 class="font-bold text-2xl text-left mb-4 ml-3">{{title}}</h2>
     <div class="flex flex-row flex-wrap items-center">
       <div v-for="(hardSource, index) of hardSources" :key="hardSource">
-        <BrowseTile :imageSource="hardSource" :channel="strings[index]"/>
+        <BrowseTile :imageSource="hardSource" :tileString="strings[index]"/>
       </div>
       <router-link to="/coming-soon" class="text-md underline text-white w-48 my-20 ml-3">see all...</router-link>
     </div>
