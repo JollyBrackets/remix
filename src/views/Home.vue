4d5572c71d94df4a557276f5e5c9527bccdca1bd
Nico <nicolas.z.zahnd@ch.pwc.com>
2021-09-25 11:34:50 +0200

    Add video player and firebase

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VideoPlayer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import VideoPlayer from '@/views/VideoPlayer.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    VideoPlayer
   }
 }
 </script>
---
+++ src/views/VideoPlayer.vue
@@ -0,0 +1,38 @@
+<template>
+  <video v-if="url" controls>
+    <source :src="url" type="video/mp4" height="200" />
+  </video>
+</template>
+
+<script>
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4HzAqy9HDGaFdSxrzwLci9ISZLDuvhlY",
+  authDomain: "remix-29f1f.firebaseapp.com",
+  projectId: "remix-29f1f",
+  storageBucket: "remix-29f1f.appspot.com",
+  messagingSenderId: "709267513332",
+  appId: "1:709267513332:web:49d147bdbffab09ea6a3a1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+export default {
+  name: 'video-player',
+  data: () => ({
+    url: null
+  }),
+  async created () {
+    this.url = await getDownloadURL(ref(storage, 'gs://remix-29f1f.appspot.com/5 cleansing breaths exercise (Headspace).mp4'))
+    console.log(this.url)
+  }
+
+}
+</script>
