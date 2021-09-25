5341b6e2ebca3f2f2d085bf99b32f06172b2bc99
maettuu <mangori41@bluewin.ch>
2021-09-25 21:02:42 +0200

    add tai chi

--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -15,8 +15,8 @@ export default {
   data: () => ({
     channelHardSources: ["caroline-girvan", "chloe-ting", "fit-for-fun", "growingannanas", "headspace", "heather-robertson", "les-mills", "mady-morrison", "my-life", "pamela-reif", "popsugar-fitness", "tiff-x-dan"],
     channels: ["Caroline Girvan", "Chloe Ting", "Fit for Fun", "Growing Annanas", "Headspace", "Heather Robertson", "Les Mills", "Mady Morrison", "My Life", "Pamela Reif", "Popsugar Fitness", "Tiff x Dan"],
-    categoriesHardSources: ["body-balance", "body-combat", "breathing", "cardio", "cool-down", "dance", "health-advice", "hiit", "meditation", "mindfullness", "pilates", "stretch", "warm-up", "yoga"],
-    categories: ["Body Balance", "Body Combat", "Breathing", "Cardio", "Cool Down", "Dance", "Health Advice", "HIIT", "Meditation", "Mindfullness", "Pilates", "Stretch", "Warm Up", "Yoga"],
+    categoriesHardSources: ["body-balance", "body-combat", "breathing", "cardio", "cool-down", "dance", "health-advice", "hiit", "meditation", "mindfullness", "pilates", "stretch", "warm-up", "yoga", "tai-chi"],
+    categories: ["Body Balance", "Body Combat", "Breathing", "Cardio", "Cool Down", "Dance", "Health Advice", "HIIT", "Meditation", "Mindfullness", "Pilates", "Stretch", "Warm Up", "Yoga", "Tai Chi"],
     muscleGroupsHardSources: ["abs", "arms", "butt", "chest", "full-body", "legs", "lower-back", "neck", "shoulders"],
     muscleGroups: ["Abs", "Arms", "Butt", "Chest", "Full Body", "Legs", "Lower Back", "Neck", "Shoulders"]
   }),
